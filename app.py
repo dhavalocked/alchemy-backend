453818337973dad590283e0772c0111121c52790
dhavalocked <prajapatidhavalv@gmail.com>
2019-03-28 17:57:20 +0530

    first push

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ photos = UploadSet('photos', IMAGES)
 # CONFIG
 app = Flask(__name__, instance_relative_config=True)
 
+from waitress import serve 
+
+
+serve(app, host='0.0.0.0', port=8080)
+
 app.config['UPLOADED_PHOTOS_DEST'] = 'static/'
 configure_uploads(app, photos)
 
